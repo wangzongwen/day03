4d262670b0b650d0481efd5e4a68ca570622e556
wzw <946906782@qq.com>
2017-12-28 11:42:38 +0800

    字符串分割测试

--- src/main/java/com/wzw/string/StringTest.java
+++ src/main/java/com/wzw/string/StringTest.java
@@ -29,4 +29,14 @@ public class StringTest {
 		boolean blank = StringUtils.isNotBlank(string);
 		System.out.println(blank);
 	}
+	
+	@Test
+	public void demo4(){
+		String string="Java string-split#test";
+		//如果一个字符串中有多个分隔符，可以用"|"作为连字符
+		String[] split = StringUtils.split(string, "|-|#");
+		for (String string2 : split) {
+			System.out.println(string2);
+		}
+	}
 }
